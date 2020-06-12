7fa54ad1e76b00230d0d39fb2dabba4850dfb6af
Sourav Baghmar <49492599+Sourav1910@users.noreply.github.com>
2020-06-12 21:58:32 +0530

    added the chat storing in db

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,36 +1,24 @@
 const express = require('express');
-const http = require('http')
-const socketio = require('socket.io');
-const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+
+const { Chat } = require('../models/chatModel');
+
 const router = express.Router();
 
-console.log("you are in chatjs route")
 router.get('/chats',async (req, res) => {
-    try{
-    io.on('connection', (socket) => {
-        console.log('New WebSocket connection')
-        console.log("socketid: "+socket.id)
+    try {
 
-        socket.emit('message', 'Welcome!')
-
-        //Sends this message to all connected clients except this one.
-        socket.broadcast.emit('message', 'A new user has joined!')
-
-        socket.on('sendMessage', (message, callback) => {
-
-            io.emit('message', message)
-            callback()
+        const chatinfo = new Chat({
+            from: req.body.type,
+            userid: req.body.userid,
+            expertid: req.body.expertid,
+            message: req.body.message
         })
+        chatinfo.save()
 
-        socket.on('disconnect', () => {
-            io.emit('message', 'A user has left!')
-        })
-    })
-}catch(error){
-    console.log(error)
-}
+    }catch(error){
+        console.log(error)
+    }
+        
 })
 
 module.exports = router;
