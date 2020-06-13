3272aaad5d1c416fa1808ad137001a885a1ed03f
Sourav Baghmar <49492599+Sourav1910@users.noreply.github.com>
2020-06-13 22:34:57 +0530

    response send to frontend from chat db

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/',async (req, res) => {
     try {
+        const messageList = [];
         console.log(req.body);
         const chatinfo = new Chat({
             sender: req.body.sender,
@@ -13,6 +14,32 @@ router.post('/',async (req, res) => {
             message: req.body.message
         })
         chatinfo.save()
+
+    const chat = await Chat.find({ "sender": req.body.sender})
+    const senderName = (await User.findById(req.body.sender)).name
+    const receiverName = (await User.findById(req.body.receiver)).name
+    console.log(chat)
+
+    for(let i=0; i<chat.length; i++){
+        const currentChat = chat[i]
+        const currentblock = {
+            "text": currentChat.message,
+            "id": currentChat._id,
+            "sender":{
+                "name":senderName,
+                "uid": currentChat.sender
+            },
+            "receiver":{
+                "name": receiverName,
+                "uid": currentChat.receiver
+            }
+        }
+        messageList.push(currentblock)
+    }
+    res.send({messageList})
+    console.log("This is the messageList")
+    console.log(messageList)
+
 
     }catch(error){
         console.log(error)
