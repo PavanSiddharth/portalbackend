5d808d911f0e4835566b559a67dd8c6ae4510e0b
PavanSiddharth <pavaneasan@gmail.com>
2020-06-17 00:48:21 +0530

    ChatBox Bug fixed

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -7,58 +7,108 @@ const router = express.Router();
 router.post('/',async (req, res) => {
     try {
         var messageList = [];
+        if(req.body.message!="")
+        {
         console.log(req.body);
         const chatinfo = new Chat({
             sender: req.body.sender,
             receiver: req.body.receiver,
             message: req.body.message
         })
-        chatinfo.save()
-
-    const chat = await Chat.find({ $or : [ { "sender": req.body.sender }, { "sender": req.body.receiver} ] })
+        chatinfo.save().then(async function(doc){
+            const chat = await Chat.find({ $or : [ { "sender": req.body.sender }, { "sender": req.body.receiver} ] })
 
     
-    const senderName = (await User.findById(req.body.sender)).name
-    const receiverName = (await User.findById(req.body.receiver)).name
-    console.log(chat)
-
-    for(let i=0; i<chat.length; i++){
-        var currentChat = chat[i]
-        var currentblock = {}
-        if(currentChat.sender == req.body.sender){
-             currentblock = {
-                "text": currentChat.message,
-                "id": currentChat._id,
-                "sender":{
-                    "name":senderName,
-                    "uid": currentChat.sender
-                },
-                "receiver":{
-                    "name": receiverName,
-                    "uid": currentChat.receiver
+            const senderName = (await User.findById(req.body.sender)).name
+            const receiverName = (await User.findById(req.body.receiver)).name
+        
+        
+            for(let i=0; i<chat.length; i++){
+                var currentChat = chat[i]
+                var currentblock = {}
+                if(currentChat.sender == req.body.sender){
+                     currentblock = {
+                        "text": currentChat.message,
+                        "id": currentChat._id,
+                        "sender":{
+                            "name":senderName,
+                            "uid": currentChat.sender
+                        },
+                        "receiver":{
+                            "name": receiverName,
+                            "uid": currentChat.receiver
+                        }
+                    }
                 }
+                else{
+                     currentblock = {
+                        "text": currentChat.message,
+                        "id": currentChat._id,
+                        "sender":{
+                            "name":receiverName,
+                            "uid": currentChat.sender
+                        },
+                        "receiver":{
+                            "name": senderName,
+                            "uid": currentChat.receiver
+                        }
+                    }
+                }
+            messageList.push(currentblock)
             }
-        }
+            res.send({messageList})
+            console.log("This is the messageList")
+            console.log(messageList)
+        
+        })}
+        
         else{
-             currentblock = {
-                "text": currentChat.message,
-                "id": currentChat._id,
-                "sender":{
-                    "name":receiverName,
-                    "uid": currentChat.sender
-                },
-                "receiver":{
-                    "name": senderName,
-                    "uid": currentChat.receiver
-                }
-            }
-        }
-    messageList.push(currentblock)
-    }
-    res.send({messageList})
-    console.log("This is the messageList")
-    console.log(messageList)
+            const chat = await Chat.find({ $or : [ { "sender": req.body.sender }, { "sender": req.body.receiver} ] })
 
+    
+            const senderName = (await User.findById(req.body.sender)).name
+            const receiverName = (await User.findById(req.body.receiver)).name
+        
+        
+            for(let i=0; i<chat.length; i++){
+                var currentChat = chat[i]
+                var currentblock = {}
+                if(currentChat.sender == req.body.sender){
+                     currentblock = {
+                        "text": currentChat.message,
+                        "id": currentChat._id,
+                        "sender":{
+                            "name":senderName,
+                            "uid": currentChat.sender
+                        },
+                        "receiver":{
+                            "name": receiverName,
+                            "uid": currentChat.receiver
+                        }
+                    }
+                }
+                else{
+                     currentblock = {
+                        "text": currentChat.message,
+                        "id": currentChat._id,
+                        "sender":{
+                            "name":receiverName,
+                            "uid": currentChat.sender
+                        },
+                        "receiver":{
+                            "name": senderName,
+                            "uid": currentChat.receiver
+                        }
+                    }
+                }
+            messageList.push(currentblock)
+            }
+            res.send({messageList})
+            console.log("This is the messageList")
+            console.log(messageList)
+        }
+
+   
 
     }catch(error){
         console.log(error)
