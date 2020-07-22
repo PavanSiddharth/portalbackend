eb75e8a2527f23039e773f180b62199036bf13b3
Ritwik Neema <ritwikneema651@gmail.com>
2020-07-22 13:14:58 +0530

    chat model fixes

--- models/chatModel.js
+++ models/chatModel.js
@@ -8,22 +8,24 @@ const chatSchema = new mongoose.Schema({
     required: true,
     default: null,
   },
-  userIds: {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      default: null,
-    },
-    messages: [
-      {
-        message: String,
-        sentOn: Date,
-        sentAt: String,
-        sentByExpert: Boolean,
+  conversations: [
+    {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        default: null,
       },
-    ],
-  },
+      messages: [
+        {
+          message: String,
+          sentOn: Date,
+          sentAt: String,
+          sentByExpert: Boolean,
+        },
+      ],
+    },
+  ],
 });
 
 module.exports = mongoose.model("Chat", chatSchema);
