a732385be7952cd55099ecbfa794659a72ff897b
PavanSiddharth <pavaneasan@gmail.com>
2020-07-29 01:39:53 +0530

    Call rating

--- models/slotModel.js
+++ models/slotModel.js
@@ -6,7 +6,9 @@ const slotSchema = new mongoose.Schema({
     userId : {type: Schema.Types.ObjectId, ref: 'User', required: true, default: null},
     Date : {type: Date, required: true },
     slot : {type: String, required: true},
-    approved : {type: Boolean, default: false}
+    approved : {type: Boolean, default: false},
+    amount : {type:Number,default:0},
+    rating : {type:Number,default:0}
 });
 
 module.exports = mongoose.model('Slot', slotSchema);
